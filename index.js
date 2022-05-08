07cfd0edfae43a0b9a3c298364518435b53b34c1
Jeremy Platt <jplatt4427@gmail.com>
2022-05-08 13:58:43 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Add your code here
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     }) 
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
+  
+
+    
+       
+       
+       //possibly need to add name and Email
+    function submitData(name, email){
+
+    const formData = {
+        name: name,
+        email: email
+        };
+        
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+        };
+    
+     return fetch("http://localhost:3000/users", configurationObject)
+     .then(function (response) {
+       return response.json();
+     })
+     .then(function (object) {
+         let par = document.createElement("p");
+         par.textContent = object.id;
+         document.body.appendChild(par);
+        
+     }) 
+     .catch(function (error) {
+         alert("There was an error");
+         //console.log(error.message);
+         let par = document.createElement("p");
+         par.textContent = error;
+         document.body.appendChild(par);
+     });
+     //return fetch("http://localhost:3000/users", configurationObject);
+    }
+  
